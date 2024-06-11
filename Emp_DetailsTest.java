3825de07195101eff6707147b96f001398ac68ff
patelzeeln <patelzeel836@gmail.com>
2024-06-11 09:25:04 +0530

    Add files via upload

---
+++ Emp_DetailsTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Emp_DetailsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
