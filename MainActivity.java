8a5b4aab078be23d0341ba0c7e5e14dab8b4e9da
patelzeeln <patelzeel836@gmail.com>
2024-06-11 09:51:09 +0530

    Add files via upload
    done

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,23 @@
-
 public class MainActivity {
+    public static void main(String[] x) {
+        // Creating an Employee object and calling its methods
+        Employee emp = new Employee("Jignesh Mevada", 21, "9173127734", "123 Elm Street", 50000.0, "Software Engineering");
+        System.out.println("Employee Details:");
+        System.out.println("Name: " + emp.getName());
+        System.out.println("Age: " + emp.getAge());
+        System.out.println("Phone Number: " + emp.getPhoneNumber());
+        System.out.println("Address: " + emp.getAddress());
+        emp.printSalary();
+        System.out.println("Specialization: " + emp.specialization);
 
-	public static void main(String[] args) {
-		
-		Area rectangle = new Area();
-
-        rectangle.setDim(5.0, 3.0);
-        
-        double area = rectangle.getArea();
-        System.out.println("The area of the rectangle is: " + area);
-
-	}
-
+        // Creating a Manager object and calling its methods
+        Manager mgr = new Manager("Vivek Solanki", 22, "0987654321", "456 Oak Avenue", 75000.0, "Human Resources");
+        System.out.println("\nManager Details:");
+        System.out.println("Name: " + mgr.getName());
+        System.out.println("Age: " + mgr.getAge());
+        System.out.println("Phone Number: " + mgr.getPhoneNumber());
+        System.out.println("Address: " + mgr.getAddress());
+        mgr.printSalary();
+        System.out.println("Department: " + mgr.department);
+    }
 }
